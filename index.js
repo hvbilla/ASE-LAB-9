de306e16615de12ae4cbf687bcb9e4902c53524f
Sai Harshavardhan Maddula <msharsha123@gmail.com>
2019-10-28 02:18:54 -0500

    lab 9

---
+++ index.js
@@ -0,0 +1,29 @@
+var reqExpress = require('express');
+var loginCookieParser = require('cookie-parser');
+var app = reqExpress();
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+app.use(reqExpress.static('public'));
+app.use(loginCookieParser());
+
+app.get('/', function(req, res){
+   res.render('index')
+});
+
+app.post('/saveCookies', function(req, res){
+   console.log("Body");
+   console.log(req.body);
+   res.cookie('loginCookie', req.body);
+   res.render('display_Cookie');
+});
+
+app.post('/displayDetails', function (req,res) {
+   res.render('display_Details',{loginCookie: req.cookies.loginCookie});
+});
+
+app.listen(3000, function(){
+   console.log("Listening to port 3000")
+});
